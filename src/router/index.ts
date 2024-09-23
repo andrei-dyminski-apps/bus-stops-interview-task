35879920a31092c1de9fcea1ce37c821bbec09cb
Andrei Dyminski <smartfox87@gmail.com>
2024-09-23 21:37:21 +0200

    set router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,24 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
-const routes: Array<RouteRecordRaw> = [];
+const TheHome = () => import("../views/the-home.vue");
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Home",
+    component: TheHome,
+  },
+  {
+    path: "/lines/:id?",
+    name: "Lines",
+    component: TheHome,
+  },
+  {
+    path: "/stops/:id?",
+    name: "Stops",
+    component: TheHome,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ src/views/the-home.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { TheBoard } from "@/features/the-board";
+</script>
+
+<template>
+  <TheBoard />
+</template>
+
+<style scoped></style>
