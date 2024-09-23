87c5835fba3fc36d6b279cbd266edac6ca5188b2
Andrei Dyminski <smartfox87@gmail.com>
2024-09-23 21:37:44 +0200

    inject styles

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "reset-css/reset.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles/main.css";
 
 createApp(App).use(store).use(router).mount("#app");
