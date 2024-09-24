bfa4096b824129d1492f1c1c90e0b7524a07540c
Andrei Dyminski <smartfox87@gmail.com>
2024-09-24 20:34:16 +0200

    update router config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,40 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { LINES_ROUTE, STOPS_ROUTE } from "@/constants/router";
 
-const TheHome = () => import("../views/the-home.vue");
+const TheLines = () => import("@/views/the-lines.vue");
+const TheStops = () => import("@/views/the-stops.vue");
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
-    component: TheHome,
-  },
-  {
-    path: "/lines/:id?",
-    name: "Lines",
-    component: TheHome,
-  },
-  {
-    path: "/stops/:id?",
-    name: "Stops",
-    component: TheHome,
+    component: () => import("@/views/the-home.vue"),
+    children: [
+      {
+        path: LINES_ROUTE,
+        name: "Lines",
+        component: TheLines,
+        children: [
+          {
+            path: `:id(\\d+)`,
+            name: "Line",
+            component: TheLines,
+          },
+        ],
+      },
+      {
+        path: STOPS_ROUTE,
+        name: "Stops",
+        component: TheStops,
+        children: [
+          {
+            path: `:id(\\d+)`,
+            name: "Stop",
+            component: TheStops,
+          },
+        ],
+      },
+    ],
   },
 ];
 
