d4d3844fec9501864a6b1ae6a6d14bb85dfefe87
Andrei Dyminski <smartfox87@gmail.com>
2024-09-26 12:53:45 +0200

    add vue-svg-loader

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.module.rule("images").exclude.add(/\.svg$/);
+
+    config.module.rules.delete("svg");
+
+    config.module
+      .rule("svg")
+      .test(/\.svg$/)
+      .use("vue-loader")
+      .loader("vue-loader")
+      .end()
+      .use("vue-svg-loader")
+      .loader("vue-svg-loader");
+  },
 });
