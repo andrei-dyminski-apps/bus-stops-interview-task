d3ec81f4d138f4b71fee8dd56e2b5c4768011bbc
Andrei Dyminski <smartfox87@gmail.com>
2024-09-26 18:30:29 +0200

    setup linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
     "eslint:recommended",
     "@vue/typescript/recommended",
     "plugin:prettier/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
   ],
   parserOptions: {
     ecmaVersion: 2020,
@@ -16,11 +18,23 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/no-v-html": "off",
+    "no-restricted-imports": ["error", { patterns: ["@/features/*/*"] }],
+    "import/no-cycle": "error",
   },
   globals: {
     defineProps: "readonly",
     defineEmits: "readonly",
     defineExpose: "readonly",
     withDefaults: "readonly",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
   },
 };
