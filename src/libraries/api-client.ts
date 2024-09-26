f30204ae2d1cc5fa36f9c5043568fc9e69f3ba74
Andrei Dyminski <smartfox87@gmail.com>
2024-09-26 22:39:50 +0200

    handle api errors

--- src/libraries/api-client.ts
+++ src/libraries/api-client.ts
@@ -1,6 +1,23 @@
 import axios from "axios";
 import { API_URL } from "@/constants/config";
+import {
+  NotificationType,
+  useNotifications,
+} from "@/features/the-notifications";
 
 export const apiClient = axios.create({
   baseURL: API_URL,
 });
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const { addNotification } = useNotifications();
+    addNotification({
+      type: NotificationType.ERROR,
+      title: "Request failed",
+      description: error.message || "Something went wrong, please try again",
+    });
+    return Promise.reject(error);
+  },
+);
