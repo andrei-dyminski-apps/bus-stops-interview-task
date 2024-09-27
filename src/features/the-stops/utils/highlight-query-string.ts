f5187a0c1f5ac0ee9a629626bc229b885f12ecb2
Andrei Dyminski <smartfox87@gmail.com>
2024-09-27 13:29:51 +0200

    fixes

--- src/features/the-stops/utils/highlight-query-string.ts
+++ src/features/the-stops/utils/highlight-query-string.ts
@@ -1,7 +1,6 @@
 export const highlightQueryString = (text: string, query: string): string => {
   if (!query?.length) return text;
   const escapedQuery = query.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
-  console.log(escapedQuery);
   return text.replace(
     new RegExp(escapedQuery, "gi"),
     (match) => `<mark>${match}</mark>`,
