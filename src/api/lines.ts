fb6a05d82993d6995ac026c7e8f31f152cf07edf
Andrei Dyminski <smartfox87@gmail.com>
2024-09-30 11:15:47 +0200

    set types

--- src/api/lines.ts
+++ src/api/lines.ts
@@ -1,6 +1,6 @@
 import { apiClient } from "@/libs/api-client";
 import type { AxiosResponse } from "axios";
-import { StopItem } from "@/types/store";
+import type { StopItem } from "@/types/store";
 
 export const getStops = (): Promise<AxiosResponse<StopItem[]>> =>
   apiClient.get(`/stops`);
